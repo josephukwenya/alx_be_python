3267c35dc774e46bdb3cd86b5f3917926ffb4c33
Joseph Ukwenya <josephukwenya.ng@gmail.com>
2025-05-23 10:10:49 +0100

    feat: basic arithmetic operations in python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,21 @@
+# Instructions:
+
+# Create a file named basic_operations.py.
+# In this file, you will define two variables: number1 and number2, with the values 10 and 5, respectively.
+# You do not need to write any functions or import any modules.
+# Calculate the sum, difference (by subtracting number2 from number1), and product of number1 and number2.
+# Print the results of each operation in the format: [operation] of [number1] and [number2] is [result].
+
+# Initializing my variables
+number1 = 10
+number2 = 5
+
+# Performing calculations
+addition_result = number1 + number2
+substraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+# Printing results
+print(f"Addition of {number1} and {number2} is {addition_result}")
+print(f"Substraction of {number1} and {number2} is {substraction_result}")
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
